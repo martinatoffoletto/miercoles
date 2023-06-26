38feab9765658f52def8074ade6c5228850be3a5
EugeFragaa <112641770+EugeFragaa@users.noreply.github.com>
2023-06-26 20:08:56 -0300

    Vistas 6

--- src/main/java/vista/Carrito.java
+++ src/main/java/vista/Carrito.java
@@ -1,14 +1,37 @@
 package vista;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Carrito {
+public class Carrito extends JFrame{
 
     private JPanel pnlCarrito;
     private JButton comprarButton;
-    private JComboBox comboBox2;
+    private JComboBox pagoComboBox2;
     private JList listaProductos;
     private JLabel lblPrecioFinal;
+    private JPanel pnlPrincipal;
+
+    public Carrito ( ) {
+        super(" Carrito");
+        //Pantalla
+        this.setContentPane(pnlPrincipal);
+        this.setSize(600, 600);
+        //this.setModal(true);
+
+        //Establezco cierre
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+
+        comprarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "Compra Realizada");
+            }
+        });
+    }
+
 
 
 }
